f24280661ec423bdfc38cc1ef9950ffe673f39ad
IlerioDef <47086473+IlerioDef@users.noreply.github.com>
2023-05-22 19:23:50 -0400

    Django Ecommerce Website | Preparing the cart | Htmx and Tailwind | Part 7 Created django app for the cart Created a cart Class Made cart globally available through context_processors.py

--- DjangoEcommerseHTMXTailwind/settings.py
+++ DjangoEcommerseHTMXTailwind/settings.py
@@ -27,10 +27,14 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+SESSION_COOKIE_AGE = 86400
+CART_SESSION_ID = 'cart'
+
 
 # Application definition
 
 INSTALLED_APPS = [
+    'cart',
     'product',
     'core',
     'django.contrib.admin',
@@ -65,6 +69,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cart.context_processors.cart',
             ],
         },
     },
