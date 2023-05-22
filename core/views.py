18e6a25992ad50dfc819c16bb86ce17a46e988bc
IlerioDef <47086473+IlerioDef@users.noreply.github.com>
2023-05-22 18:40:01 -0400

    Django Ecommerce Website | Categories/Search | Htmx and Tailwind | Part 5 Made categories shown in the sidebar Made active category shown Made simple product search

--- core/views.py
+++ core/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-
-from product.models import Product
+from django.db.models import Q
+from product.models import Product, Category
 
 
 def frontpage(request):
@@ -11,8 +11,22 @@ def frontpage(request):
 
 
 def shop(request):
+    categories = Category.objects.all()
     products = Product.objects.all()
 
-    return render(request, 'core/shop.html', {
+    active_category = request.GET.get('category', '')
+
+    if active_category:
+        products = products.filter(category__slug=active_category)
+
+    query = request.GET.get('query', '')
+    if query:
+        products = products.filter(Q(name__icontains=query) | Q(description__icontains=query))
+
+    context = {
+        'categories': categories,
         'products': products,
-    })
+        'active_category': active_category,
+    }
+
+    return render(request, 'core/shop.html', context)
