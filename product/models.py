f91c53464a7fcb11c621ffc4ee189462fca821a2
IlerioDef <47086473+IlerioDef@users.noreply.github.com>
2023-05-22 21:30:22 -0400

    Django Ecommerce Website | Sign up/Log in HTML | Htmx and Tailwind | Part 9 Set up html for the sign up page Set up html for the log in page Added product images

--- product/models.py
+++ product/models.py
@@ -1,4 +1,7 @@
+from io import BytesIO
+from django.core.files import File
 from django.db import models
+from PIL import Image
 
 
 class Category(models.Model):
@@ -19,6 +22,8 @@ class Product(models.Model):
     description = models.TextField(blank=True, null=True)
     price = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='uploads/', blank=True, null=True)
+    thumbnail = models.ImageField(upload_to='uploads/', blank=True, null=True)
 
     class Meta:
         ordering = ('-created_at',)
@@ -26,6 +31,31 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
-
     def get_display_price(self):
         return self.price / 100
+
+    def get_thumbnail(self):
+        if self.thumbnail:
+            return self.thumbnail.url
+        else:
+            if self.image:
+                self.thumbnail = self.make_thumbnail(self.image)
+                self.save()
+
+                return self.thumbnail.url
+
+            else:
+                return 'https://placehold.jp/240x240.png'
+
+    def make_thumbnail(self, image, size=(300, 300)):
+        img = Image.open(image)
+        img.convert('RGB')
+        img.thumbnail(size)
+
+        thumb_io = BytesIO()
+        img.save(thumb_io, 'JPEG', quality=85)
+
+        thumbnail = File(thumb_io, name=image.name)
+
+        return thumbnail
+
