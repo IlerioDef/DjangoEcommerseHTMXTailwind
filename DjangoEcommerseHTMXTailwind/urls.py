0fcb8b7fb5a001f53c2357e5eeabfdea2250a094
IlerioDef <47086473+IlerioDef@users.noreply.github.com>
2023-05-22 20:01:37 -0400

    Django Ecommerce Website | Add to the cart | Htmx and Tailwind | Part 8 Created view for adding to cart Included htmx cdn (script) made Add to cart functionality

--- DjangoEcommerseHTMXTailwind/urls.py
+++ DjangoEcommerseHTMXTailwind/urls.py
@@ -4,10 +4,12 @@ from django.urls import path
 
 from core.views import frontpage, shop
 from product.views import product
+from cart.views import add_to_cart
 
 urlpatterns = [
     path('', frontpage, name="frontpage"),
     path('shop/', shop, name='shop'),
     path('admin/', admin.site.urls),
-    path('shop/<slug:slug>/', product, name='product')
+    path('shop/<slug:slug>/', product, name='product'),
+    path('add_to_cart/<int:product_id>/', add_to_cart, name='add_to_cart')
 ]
