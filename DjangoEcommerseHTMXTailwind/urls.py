906f3916656d26752ddeff433107ae70610c7020
IlerioDef <47086473+IlerioDef@users.noreply.github.com>
2023-05-23 22:04:43 -0400

    Django Ecommerce Website | Refactoring - 1 | Htmx and Tailwind | Part 12 Add to cart from search/lists Checking if image exists first (product detail) Fixed links in the menu Separated URL files Added title to the browser

--- DjangoEcommerseHTMXTailwind/urls.py
+++ DjangoEcommerseHTMXTailwind/urls.py
@@ -1,21 +1,10 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
-from django.contrib.auth import views
-from core.views import frontpage, shop, signup
-from product.views import product
-from cart.views import add_to_cart, cart, checkout
+from django.urls import path, include
 
 urlpatterns = [
-    path('signup/', signup, name='signup'),
-    path('logout/', views.LogoutView.as_view(), name='logout'),
-    path('login/', views.LoginView.as_view(template_name='core/login.html'), name='login'),
-    path('', frontpage, name="frontpage"),
-    path('shop/', shop, name='shop'),
     path('admin/', admin.site.urls),
-    path('shop/<slug:slug>/', product, name='product'),
-    path('cart/', cart, name='cart'),
-    path('cart/checkout/', checkout, name='checkout'),
-    path('add_to_cart/<int:product_id>/', add_to_cart, name='add_to_cart')
+    path('', include('core.urls')),
+    path('cart/', include('cart.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
