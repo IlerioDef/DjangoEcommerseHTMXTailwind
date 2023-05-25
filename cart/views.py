7fd5be0f3f1e65c4472f7efaa130a2e7fb575cba
IlerioDef <47086473+IlerioDef@users.noreply.github.com>
2023-05-24 23:17:38 -0400

    Django Ecommerce Website | Finish checkout | Htmx and Tailwind | Part 18 Errors are shown in the checkout Changed ordering for orders Simple success page created Cart can be cleared Prices in orders page are correct

--- cart/views.py
+++ cart/views.py
@@ -17,6 +17,10 @@ def cart(request):
     return render(request, 'cart/cart.html')
 
 
+def success(request):
+    return render(request, 'cart/success.html')
+
+
 def update_cart(request, product_id, action):
     cart = Cart(request)
 
